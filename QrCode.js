788eacd0910e76911f36fc47c644ea130fa645f0
NareshKumar1553 <nareshkumar.s1553@gmail.com>
2022-12-26 20:25:07 +0530

    commit

--- QrCode.js
+++ QrCode.js
@@ -4,7 +4,7 @@ const QrCode= ()=>{
     return(
         <View style={{flexDirection: 'column',flex:1,justifyContent:'center',alignSelf:'center'}}>
             <Text style={{color:'black',alignSelf:'center',fontWeight:'bold',texttransform:'capitalize',fontSize:24,paddingBottom:30}}>QrCode Authendication</Text>
-            <ImageBackground source={require('./Images/QRScan.png')} style={{width: 350, height: 350}}/>
+            <ImageBackground source={require('./Images/QRScan.png')} style={{width: 250, height: 350}}/>
         </View>
     )
 }
